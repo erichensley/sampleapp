88d3534b6b3d1d546de648931e1155fb54bf50c0
Eric Hensley <eric.hensley@gmail.com>
2011-05-12 20:51:53 -0700

    Completed the layout structure

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,7 +4,8 @@ describe PagesController do
   render_views
   
   before(:each) do
-    @base_title = "Ruby on Rails Tutorial Samples App"
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
   
   describe "GET 'home'" do
     it "should be successful" do
