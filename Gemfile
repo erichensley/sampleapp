7ec511f34dfd91f597d06a3563b84ed879596eb3
Eric Hensley <eric.hensley@gmail.com>
2011-05-22 15:52:02 -0700

    Added a User factor and tests for the show page

--- Gemfile
+++ Gemfile
@@ -16,4 +16,5 @@ end
 group :test do
   gem 'rspec'
   gem 'webrat'
+  gem 'factory_girl_rails', '1.0'
 end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Eric H"
+  user.email                 "bob@bob.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
